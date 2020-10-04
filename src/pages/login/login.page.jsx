c4a1caea8f70f757755344b290584b758f7167b1
ronyahmmod <rony.jib@gmail.com>
2020-10-04 14:14:29 +0600

    Seperate Client file

---
+++ src/pages/login/login.page.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import axiso from 'axios';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '-1',
+      password: '-1',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let res;
+    const sendPostRequest = async () => {
+      res = await axiso({
+        url: 'http://localhost:2020/api/v1/users/signin',
+        method: 'post',
+        data: {
+          email: this.state.email,
+          password: this.state.password,
+        },
+      });
+    };
+    sendPostRequest();
+    console.log(res);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form method="post" action="http://localhost:2020/api/v1/users/signin">
+          <div className="col-sm-8 col-md-6 m-auto">
+            <h3 className="display-3 text-center">SIGN IN</h3>
+            {this.state.email ? (
+              ''
+            ) : (
+              <div className="alert alert-danger">Enter Email</div>
+            )}
+            <div className="form-group">
+              <label htmlFor="email">Email Address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                onChange={this.handleChange}
+                placeholder="Enter email"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Email Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                onChange={this.handleChange}
+                placeholder="Enter password"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              name="submit"
+              className="btn btn-primary"
+              onSubmit={this.handleSubmit}
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
